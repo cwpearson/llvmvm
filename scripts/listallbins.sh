b3b3c39c46b3dded14050cc05b154ed2a1d91dd1
Carl Pearson <pearson@illinois.edu>
2017-03-07 21:54:28 -0600

    List binary releases for llvm+clang

--- scripts/listallbins.sh
+++ scripts/listallbins.sh
@@ -7,30 +7,13 @@ set -eou pipefail
 URL="http://releases.llvm.org/download.html"
 HTML=`curl -s $URL`
 ALL_LINKS=`echo "$HTML" | grep href`
-CLANG_LINKS=`echo "$ALL_LINKS" | grep -Eo "[[:digit:].]*/(llvm|clang)\+(llvm|clang)[^>]*.(g|x)z"`
+CLANG_LINKS=`echo "$ALL_LINKS" | grep -Eo "(llvm|clang)\+(llvm|clang)[^>]*.(g|x)z"`
 
-echo "$CLANG_LINKS"
-
-# GREP_URL_DESC_RE="clang"
-# SUPPORTED_TAG_RE="RELEASE_"
-# LIST_RE="final|rc[[:digit:]]+"
-
-# for tag in $TAGS; do
-#   if [[ $tag =~ $SUPPORTED_TAG_RE ]]; then
-#     SUBDIRS=`svn ls $URL/$tag`
-#     PRINT_ROOT=1
-#     for d in $SUBDIRS; do
-#       if [[ "$tag$d" =~ $LIST_RE ]]; then
-#         PRINT_ROOT=0
-#         llvmvm_tag_to_id "$tag$d"
-#         echo "$result"
-#       fi
-#     done
-#     if [[ $PRINT_ROOT == 1 ]]; then
-#       echo "$tag"
-#     fi
-#   else
-#    :
-#     # echo "skip" $tag
-#   fi
-# done
+for link in $CLANG_LINKS; do
+  BASE_RE="(.*)\.(tar.gz|xz|tgz)"
+  if [[ "$link" =~ $BASE_RE ]]; then
+    echo ${BASH_REMATCH[1]};
+  else
+    llvmvm_display_fatal "Unhandled binary link $link"
+  fi
+done
