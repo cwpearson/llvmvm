38c267f6956b70defbbd98dc2ba1c05b9609133e
Carl Pearson <pearson@illinois.edu>
2017-03-09 13:56:07 -0600

    More debug for travis

--- scripts/functions/llvmvm_export_path.sh
+++ scripts/functions/llvmvm_export_path.sh
@@ -4,15 +4,22 @@ function llvmvm_export_path() {
     echo "in llvmvm_export_path bash function"
     echo "with args $@"
 
+    echo "Path before:"
+    echo $PATH
+
+    # Remove prior llvmvm-related paths from the path
     path_lines=`echo "$PATH" | tr ":" "\n" | "$GREP_PATH" -v '^$'`
     path_without_llvmvm=`echo "$path_lines" | egrep -v "$LLVMVM_ROOT/(llvms|bin)" | tr "\n" ":" | sed 's/:*$//'`
     echo "cleaned path"
 
+    echo $PATH
+
+    # Add llvmvm back to the path
 	export PATH="$LLVMVM_ROOT/bin:$path_without_llvmvm"
     export LLVMVM_PATH_BACKUP="$PATH"
 
+    # Add the requested llvm to the path
     llvmvm_get_path_for_name "$1"
     local path="$result/ins/bin"
-
     export PATH="$path:$PATH"
 }
