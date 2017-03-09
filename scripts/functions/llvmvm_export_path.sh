7472a6b5ea6ff3d374ef7620836c5a24cb815b10
Carl Pearson <pearson@illinois.edu>
2017-03-09 14:17:14 -0600

    make export path quieter

--- scripts/functions/llvmvm_export_path.sh
+++ scripts/functions/llvmvm_export_path.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
 function llvmvm_export_path() {
-    echo "in llvmvm_export_path bash function"
-    echo "with args $@"
-
-    echo "Path before:"
-    echo $PATH
+    # echo "in llvmvm_export_path bash function"
+    # echo "with args $@"
 
     # Remove prior llvmvm-related paths from the path
     path_lines=`echo "$PATH" | tr ":" "\n" | grep -v '^$'`
     path_without_llvmvm=`echo "$path_lines" | egrep -v "$LLVMVM_ROOT/(llvms|bin)" | tr "\n" ":" | sed 's/:*$//'`
-    echo "cleaned path"
-
-    echo $PATH
 
     # Add llvmvm back to the path
 	export PATH="$LLVMVM_ROOT/bin:$path_without_llvmvm"
