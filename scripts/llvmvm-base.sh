1f69906266c20f863b2d5a649f6387de750bfe01
Carl Pearson <pearson@illinois.edu>
2017-03-09 07:01:49 -0600

    Finish first pass llvmvm installer script

---
+++ scripts/llvmvm-base.sh
@@ -0,0 +1,7 @@
+
+export PATH="$LLVMVM_ROOT/bin:$PATH"
+export LLVMVM_PATH_BACKUP="$PATH"
+
+[ -f "$LLVMVM_ROOT/environments/default" ] && . "$LLVMVM_ROOT/environments/default"
+
+. "$LLVMVM_ROOT/scripts/env/llvmvm.sh"
--- scripts/llvmvm.sh
+++
@@ -1,9 +0,0 @@
-unset LLVMVM_ROOT
-
-export LLVMVM_ROOT="$HOME/.llvmvm"
-export PATH="$LLVMVM_ROOT/bin:$PATH"
-export LLVMVM_PATH_BACKUP="$PATH"
-
-# [ -f "$LLVMVM_ROOT/environments/default" ] && . "$LLVMVM_ROOT/environments/default"
-
-. "$LLVMVM_ROOT/scripts/env/llvmvm.sh"
